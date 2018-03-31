7c2042b4b9a12f536ff3eefa3aaabe871359ec05
bastlenoob <bastien.labouche@live.fr>
2018-03-31 16:02:04 +0200

    Ajout lecture des ingredients de la bdd

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'RecipeController';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['recipe'] = 'RecipeController';
+$route['recipe'] = 'RecipeController/recipe';
 $route['recipe/(:num)'] = 'RecipeController/recipe/$1';
+$route['ing'] = 'IngController';
